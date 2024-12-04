88bd533227eed3b1c17f49e76b4a10570adc4a24
mathisdesraches <mathis.desraches@edu.univ-fcomte.fr>
2024-12-04 09:43:41 +0100

    html

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,41 @@
-// *** LIGHTBOX ***
+document.addEventListener('DOMContentLoaded', () => {
+    const images = document.querySelectorAll('ul img');
+    let expandedImage = null;
+  
+    images.forEach(img => {
+      img.addEventListener('click', () => {
+        if (img === expandedImage) {
+       
+          img.classList.remove('expanded');
+          document.body.classList.remove('expanding');
+          expandedImage = null;
+  
+         
+          img.src = img.dataset.thumbnail;
+        } else {
+          
+          if (expandedImage) {
+            expandedImage.classList.remove('expanded');
+            expandedImage.src = expandedImage.dataset.thumbnail; 
+          }
+  
+          img.src = img.dataset.fullImg;
+          img.classList.add('expanded');
+          document.body.classList.add('expanding');
+          expandedImage = img;
+        }
+      });
+    });
+  
+   
+    document.body.addEventListener('click', (e) => {
+      if (expandedImage && !e.target.closest('ul img')) {
+        expandedImage.classList.remove('expanded');
+        document.body.classList.remove('expanding');
+        expandedImage.src = expandedImage.dataset.thumbnail;
+        expandedImage = null;
+      }
+    });
+  });
+  
+  
